ab0b63947065af51fbe2a9c2be7d29c718b4452c
José Ciriaco <103939971+Izeto@users.noreply.github.com>
2022-10-03 11:55:57 -0300

    praticando

---
+++ revisao/src/Calculadora.java
@@ -0,0 +1,23 @@
+public class Calculadora {
+    public void calcSoma(int num1, int num2){
+        int soma = num1+num2;
+        System.out.println(soma);
+    }
+    public void calcMult(int num1, int num2){
+        int mult = num1*num2;
+        System.out.println(mult);
+    }
+    public void calcRaiz(int num1){
+        int raiz = (int) Math.sqrt(num1);
+        System.out.println(raiz);
+    }
+
+    public void calcPow(int num1, int num2){
+        int pow = (int) Math.pow(num1,num2);
+        System.out.println(pow);
+    }
+    public void calcTabu(int num1, int num2){
+        int soma = num1+num2;
+            System.out.println(num1 "x" num2 "=" resulTabu);
+    }
+}
---
+++ revisao/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Calculadora calculadora = new Calculadora();
+        calculadora.calcMult(2,4);
+    }
+}
